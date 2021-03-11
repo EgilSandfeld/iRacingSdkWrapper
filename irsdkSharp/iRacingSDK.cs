2801bc778ed274ce979dd4dadd49166682224483
Egil Sandfeld (Julle) <sandfeld@gmail.com>
2021-03-11 21:22:11 +0100

    Updated YamlDotNet to 9.1.4

--- irsdkSharp/iRacingSDK.cs
+++ irsdkSharp/iRacingSDK.cs
@@ -1,10 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.IO.MemoryMappedFiles;
+using System.Text;
 
 namespace iRSDKSharp
 {
