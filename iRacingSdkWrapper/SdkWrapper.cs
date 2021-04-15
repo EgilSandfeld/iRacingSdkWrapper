43946298ed96ef772091375ee21c8fafce1da171
Egil Sandfeld (Julle) <sandfeld@gmail.com>
2021-04-15 16:03:30 +0200

    ...

--- iRacingSdkWrapper/SdkWrapper.cs
+++ iRacingSdkWrapper/SdkWrapper.cs
@@ -466,6 +466,7 @@ namespace iRacingSdkWrapper
 
             csvContent.AppendLine("Variable,PlayerValue" + zeroTo63);
 
+
             foreach (var header in Sdk.VarHeaders)
             {
                 try
