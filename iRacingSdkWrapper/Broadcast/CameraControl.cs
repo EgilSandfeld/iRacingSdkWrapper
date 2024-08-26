8bb8b701444fd378eb797268c3a2959ffc718425
egil_sandfeld <sandfeld@gmail.com>
2024-08-26 09:28:17 +0200

    Removed unused references

--- iRacingSdkWrapper/Broadcast/CameraControl.cs
+++ iRacingSdkWrapper/Broadcast/CameraControl.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using iRacingSdkWrapper.Bitfields;
+﻿using iRacingSdkWrapper.Bitfields;
 using iRSDKSharp;
 
 namespace iRacingSdkWrapper.Broadcast
